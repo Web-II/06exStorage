5e8a7930c9bf905b0cf9509bae4857e9a2e84972
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2023-03-31 09:44:09 +0200

    Actuele jaren

--- oefening2/js/GefietsteKilometersRepository.js
+++ oefening2/js/GefietsteKilometersRepository.js
@@ -15,18 +15,19 @@ export default class GefietsteKilometersRepository {
   #voegToe(jaar, aantalKilometers, bedragPerKilometer) {}
 
   #opvullen() {
-    this.#voegToe(
-      2020,
+    const huidigJaar = new Date().getFullYear();
+    this.voegToe(
+      huidigJaar - 2,
       [107, 109, 183, 154, 118, 136, 104, 178, 189, 98, 107, 145],
       0.2
     );
-    this.#voegToe(
-      2021,
+    this.voegToe(
+      huidigJaar - 1,
       [123, 145, 178, 113, 174, 158, 149, 133, 167, 120, 166, 142],
       0.2
     );
-    this.#voegToe(
-      2022,
+    this.voegToe(
+      huidigJaar,
       [156, 128, 129, 160, 190, 145, 155, 198, 120, 130, 140, 150],
       0.21
     );
